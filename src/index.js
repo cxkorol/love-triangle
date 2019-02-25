df6421b6e7226889b030ed281f9f2e91f947f3c8
korol <cx.korol@gmail.com>
2019-02-25 21:50:45 +0300

    Solving task Love Triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    preferences.unshift(0);
+    
+    for (let i = 1; i < preferences.length; i++) {
+      let j = preferences[i];
+      let k = preferences[j];
+      count += i == preferences[k];
+    }
+  return Math.floor(count / 3);
 };
